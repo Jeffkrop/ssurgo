ccdaf3a3d510ad2b960b222f14e6e0cea6aab5d5
Jeffkrop <jeffkrop@gmail.com>
2018-01-30 14:24:52 -0600

    first upload

---
+++ ssurgo_r.R
@@ -0,0 +1,19 @@
+---
+  title: "SSURGO Data"
+date: "`r format(Sys.Date())`"
+output: github_document
+---   
+  
+  
+  ```{r, message=FALSE, warning=FALSE, echo=FALSE}
+library(knitr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(readr)
+library(maps)
+```   
+
+```{r, message=FALSE, warning=FALSE, echo=FALSE}
+<- read_csv("")
+```
