d06b00241899cf6bb17e7a644a6ced91e0ad2f53
Jeffkrop <jeffkrop@gmail.com>
2018-02-02 13:58:08 -0600

    Working code to download soil type for any location in the USA.

--- ssurgo_r.R
+++ ssurgo_r.R
@@ -1,9 +1,4 @@
----
-  title: "SSURGO Data"
-date: "`r format(Sys.Date())`"
 
-
-  
   
 library(readxl)
 library(knitr)
@@ -14,38 +9,42 @@ library(maps)
 library(soilDB)   
 library(sp)
 
+
+#From prisum the dataset is 1266 rows
 # I have to remove 280 from this dataset if I want soil data.
-#114 locations have no latitude or longitude
+#142 locations have no latitude or longitude
 #57 are duplicates
 #Canada: 81 locations 
 #Mexico: 28 locations 
 #Both have been removed from this data.
 
-insight <- read_excel("insight.xlsx")
+insight <- read.csv("")
 insight <- insight %>% select(TrialName, GrowerName, latitude = FarmLatitude, longitude = FarmLongitude, GrowerState) %>% filter(!is.na(latitude), !is.na(longitude), longitude != -57.86900)
 
 #Remove duplicate
 insight <- insight[-which(duplicated(insight$latitude)), ]   
 
 # There are 12 locations where the latitude and longatude are fliped below code with find it fix it and remove it.
-fix <- insight %>% filter(latitude < 5) %>% 
+fix_flipped <- insight %>% filter(latitude < 5) %>% 
                    select(TrialName, GrowerName, latitude = longitude, longitude = latitude, GrowerState)
 
-
-insight <- rbind(fix, insight)
+insight <- rbind(fix_flipped, insight)
 insight <- insight[ ! ( ( insight$latitude < 5)) , ]
 
+fix_neg <- insight %>% filter(longitude > 1) %>% mutate(longitude = longitude * -1)
+insight <- rbind(fix_neg, insight) 
+insight <- insight[ ! ( ( insight$longitude > 1)) , ]
+
 #Fix two longitudes that are missing the negitive sign.
-insight$longitude[ insight$longitude  ==  98.21478 ] <- -98.21478
-insight$longitude[ insight$longitude  ==  107.07168 ] <- -107.07168
+insight$latitude[ insight$latitude  ==  442440.21000 ] <- 44.244021
+insight$longitude[ insight$longitude  ==  -95102.52000 ] <- -95.10252
+
 
 #Add a column for ID to join to after I get soil data.
 insight <- tibble::rowid_to_column(insight, "ID")
 
-
-
 #Save as csv as clean data if I need to map it with ArcGIS
-write_csv(insight, "clean_insight.csv" )
+#write_csv(insight, "clean_soybean_insight.csv" )
 
 
 #Get a map of Canada, Mexico and the USA
@@ -57,8 +56,8 @@ usa_map <- map_data("state")
 # Draw the map and add the data points in myData
 ggplot() +
   geom_path(data = usa_map, aes(x = long, y = lat, group = group)) +
-  #geom_path(data = world_map, aes(x = long, y = lat, group = group)) +
-  geom_point(data = clean_soil, aes(x = longitude, y = latitude), color = "red") +
+  geom_path(data = world_map, aes(x = long, y = lat, group = group)) +
+  geom_point(data = insight, aes(x = longitude, y = latitude), color = "red") +
   labs("Origin of Cocoa Beans", x = "Longitude", y = "Latitude") +
   theme(plot.title=element_text(size=20))    
 
@@ -67,6 +66,7 @@ ggplot() +
 #Mexico: (loc 11) = 2, (loc 5) = 12, (loc 8) = 14 means we lose 28 locations 
 locations <- insight %>% filter(GrowerState != "QC", GrowerState != "11", GrowerState != "5", GrowerState != "8", GrowerState != "ON") %>% select(ID, latitude, longitude, GrowerName)
 
+#locations <- locations[1:2,]
 #Data frame of lat lon
 xy <- locations[,c(3,2)]
 
@@ -74,7 +74,7 @@ xy <- locations[,c(3,2)]
 soil <- SpatialPointsDataFrame(coords = xy, data = locations,
                                proj4string = CRS("+proj=longlat +datum=WGS84"))
 
-#Get soil data
+#Get soil data https://cran.r-project.org/web/packages/soilDB/soilDB.pdf
 system.time(soil <-SDA_query_features(soil, id='ID'))
 
 
@@ -85,12 +85,15 @@ mid_soil <- soil %>% separate(muname, c("soil", "sand", "slope", "con"), ", ") %
 
 mid_soil  <- left_join(mid_soil, locations, by = "ID")
 
+#write.csv(mid_soil, "not_clean_insight_soil_data.csv")
+
+
 #Rename to save
 clean_soil <- mid_soil
 
-
+clean_soil$soil <- gsub("\\s*\\([^\\)]+\\)\\s*$","",clean_soil$soil)
 clean_soil$soil[clean_soil$soil == "loams"] <- "loam"
-
+clean_soil$soil <- gsub("clays", "clay", clean_soil$soil)
 clean_soil$soil <- gsub("loams", "loam", clean_soil$soil)
 clean_soil$soil <- gsub("very ", "", clean_soil$soil)
 clean_soil$soil <- gsub("and ", "", clean_soil$soil)
@@ -128,6 +131,15 @@ clean_soil$soil <- gsub("Sueur-Lester ", "", clean_soil$soil)
 clean_soil$soil <- gsub("Sullivan ", "", clean_soil$soil)
 clean_soil$soil <- gsub("Twin-Walnut grove ", "", clean_soil$soil)
 clean_soil$soil <- gsub("variant ", "", clean_soil$soil)
+clean_soil$soil <- gsub("Page-McIntosh ", "", clean_soil$soil)
+clean_soil$soil <- gsub("Desha ", "", clean_soil$soil)
+clean_soil$soil <- gsub("land-Udorthents ", "", clean_soil$soil)
+clean_soil$soil <- gsub("Monongahela ", "", clean_soil$soil)
+clean_soil$soil <- gsub("Poxmash ", "", clean_soil$soil)
+clean_soil$soil <- gsub("Prairie ", "", clean_soil$soil)
+clean_soil$soil <- gsub("Shelby ", "", clean_soil$soil)
+clean_soil$soil <- gsub(" (occasionally flooded)", "", clean_soil$soil)
+clean_soil$soil <- gsub("Uly ", "", clean_soil$soil)
 
 clean_soil <- clean_soil %>% filter(soil != "(Nauvoo) fine sandy loam",
                                     soil != "soils",
@@ -137,10 +149,51 @@ clean_soil <- clean_soil %>% filter(soil != "(Nauvoo) fine sandy loam",
                                     soil != "or quarry",
                                     soil != "pits",
                                     soil != "Purdam soils",
-                                    soil != "Rexor soils")
+                                    soil != "Rexor soils",
+                                    soil !=  "Cathro")
+
+#write.csv(clean_soil, "clean_insight_soil_data.csv")
+
+#After removing 33 locations that do not return good sail data we are left with 925
+
+
 
 n4 <- clean_soil %>% group_by(soil) %>%
              summarise(count = n()) %>%
              arrange(desc(count))
+
+
+map_soil <- clean_soil 
+map_soil$soil_texture <- map_soil$soil
+map_soil$soil_texture[ map_soil$soil_texture ==  "loam" ] <- "medium"
+map_soil$soil_texture[ map_soil$soil_texture ==  "silt loam" ] <- "medium"
+map_soil$soil_texture[ map_soil$soil_texture ==  "silty clay loam" ] <- "fine"
+map_soil$soil_texture[ map_soil$soil_texture ==  "complex" ] <- "medium"
+map_soil$soil_texture[ map_soil$soil_texture ==  "clay loam" ] <- "fine"
+map_soil$soil_texture[ map_soil$soil_texture ==  "fine sandy loam" ] <- "coarse"
+map_soil$soil_texture[ map_soil$soil_texture ==  "sandy loam" ] <- "coarse"
+map_soil$soil_texture[ map_soil$soil_texture ==  "loamy fine sand" ] <- "coarse"
+map_soil$soil_texture[ map_soil$soil_texture ==  "clay" ] <- "fine"
+map_soil$soil_texture[ map_soil$soil_texture ==  "loamy sand" ] <- "coarse"
+map_soil$soil_texture[ map_soil$soil_texture ==  "silty clay" ] <- "fine"
+map_soil$soil_texture[ map_soil$soil_texture ==  "fine sand" ] <- "coarse"
+map_soil$soil_texture[ map_soil$soil_texture ==  "sand" ] <- "coarse"
+map_soil$soil_texture[ map_soil$soil_texture ==  "muck" ] <- "fine"
+map_soil$soil_texture[ map_soil$soil_texture ==  "Black clay" ] <- "fine"
+map_soil$soil_texture[ map_soil$soil_texture ==  "sandy clay loam" ] <- "medium"
+
+map_soil <- map_soil %>% select(GrowerName, soil, soil_texture, latitude, longitude)
+write_csv(map_soil, "soybean insight soil data.csv")
+
+
+n5 <- map_soil %>% group_by(soil_texture) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count))
+
+
+
+
+
+
 
 
